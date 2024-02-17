bca274a818bd30e6f254bba1cebe372673165095
Girish Nair <nairgirish100@gmail.com>
2024-02-17 18:55:13 +0400

    moer updates

--- tests/locators.spec.js
+++ tests/locators.spec.js
@@ -2,12 +2,8 @@ import { test, expect } from "@playwright/test";
 
 
 test('Working with different locators', async ({page}) => {
-    await page.goto('https://www.amazon.ae/')
-    await page.getByPlaceholder('Search Amazon.ae').fill('lego technic')
+    await page.goto('https://amazon.in/')
+    await page.getByPlaceholder('Search Amazon.in').fill('lego technic')
     await page.click('#nav-search-submit-button')
-    await page.getByText('Mobile Phones').click()
-    
-    await page.goto('https://demo.nopcommerce.com/login?returnUrl=%2F')
-    await page.getByLabel('Email').fill('admin@test.com')
-    
+    await page.getByText('Mobiles').click()
 })
